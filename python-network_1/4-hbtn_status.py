7dcbdc45379021cf8f786553fb37b6a3a5b3eadd
Noella799 <n.uwera@alustudent.com>
2025-07-31 11:11:28 -0700

    upgraded 44

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,13 +1,26 @@
+
 #!/usr/bin/python3
 """
-Fetches https://alu-intranet.hbtn.io/status using requests package
-and displays the response body information.
+Fetches status from either:
+- https://intranet.hbtn.io/status (primary)
+- http://0.0.0.0:5050/status (fallback)
+using requests package and displays response information.
 """
 
 import requests
 
 if __name__ == "__main__":
-    response = requests.get('https://alu-intranet.hbtn.io/status')
-    print("Body response:")
-    print("\t- type: {}".format(type(response.text)))
-    print("\t- content: {}".format(response.text))
+    urls = [
+        'https://intranet.hbtn.io/status',
+        'http://0.0.0.0:5050/status'
+    ]
+    
+    for url in urls:
+        try:
+            response = requests.get(url)
+            print("Body response:")
+            print("\t- type: {}".format(type(response.text)))
+            print("\t- content: {}".format(response.text))
+            break
+        except requests.exceptions.RequestException:
+            continue
