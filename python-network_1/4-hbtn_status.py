2cd17b1d432e2fde978d6631c828d6331b268d69
Noella799 <n.uwera@alustudent.com>
2025-07-31 11:22:46 -0700

    upgraded 44

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,26 +1,10 @@
-
 #!/usr/bin/python3
-"""
-Fetches status from either:
-- https://intranet.hbtn.io/status (primary)
-- http://0.0.0.0:5050/status (fallback)
-using requests package and displays response information.
-"""
+"""Python script that fetchs."""
+
 
 import requests
-
 if __name__ == "__main__":
-    urls = [
-        'https://intranet.hbtn.io/status',
-        'http://0.0.0.0:5050/status'
-    ]
-    
-    for url in urls:
-        try:
-            response = requests.get(url)
-            print("Body response:")
-            print("\t- type: {}".format(type(response.text)))
-            print("\t- content: {}".format(response.text))
-            break
-        except requests.exceptions.RequestException:
-            continue
+    requ = requests.get('https://intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t- type: {}".format(type(requ.text)))
+    print("\t- content: {}".format(requ.text))
