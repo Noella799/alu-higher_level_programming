d1381a3ad6dc8d1c902ea4471e3e354a22cfb6cd
Noella799 <n.uwera@alustudent.com>
2025-07-31 10:42:14 -0700

    upgraded 00

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-Python script that fetches https://intranet.hbtn.io/status
-using the urllib package and displays the response body.
+Fetches https://intranet.hbtn.io/status using urllib package
+and displays specific information about the response body.
 """
 
 import urllib.request
@@ -9,12 +9,9 @@ import urllib.request
 if __name__ == "__main__":
     url = 'https://intranet.hbtn.io/status'
     
-    try:
-        with urllib.request.urlopen(url) as response:
-            content = response.read()
-            print("Body response:")
-            print("\t- type: {}".format(type(content)))
-            print("\t- content: {}".format(content))
-            print("\t- utf8 content: {}".format(content.decode('utf-8')))
-    except urllib.error.URLError as e:
-        print("Error:", e.reason)
+    with urllib.request.urlopen(url) as response:
+        content = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))  # Removed b'' formatting
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
